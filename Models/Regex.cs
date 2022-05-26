0626621aff497cbb45043da909fa3de1a0b57b64
sSs-42 <78847037+sSs-42@users.noreply.github.com>
2022-05-27 00:51:18 +0700

    Add files via upload

---
+++ Models/Regex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace RegexApp1.Models
+{
+    public class MyRegex
+    {
+
+        public static string? FindRegexInText(string text, string pattern)
+        {
+            string result = "";
+            if (text == null || text == "")
+            {
+                return result;
+            }
+
+            if (pattern == null || pattern == "")
+            {
+                return text;
+            }
+            Regex r = new Regex(pattern);
+            MatchCollection m = r.Matches(text);
+            foreach (Match x in m)
+            {
+                result += (x.Value + "\n");
+            }
+            return result;
+        }
+    }
+}
